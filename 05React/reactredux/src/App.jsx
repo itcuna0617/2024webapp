7635efea060f3650bac6286d1eab6dec191e40ab
itcuna0617 <itcuna0617@gmail.com>
2024-09-19 15:03:20 +0900

    2024.09.19

--- 05React/reactredux/src/App.jsx
+++ 05React/reactredux/src/App.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { createStore } from "redux";
 
+// 원래는 이렇게 쓰지 않음
 const initalData = {
   cart: [],
 };
 
+// react의 useReducer와 비슷한 구조로 작성한 것
+// reducer의 목적 : 이 안에 script를 작성해둬서 관리하기 편하게 하기 위해서(데이터가 어디서 변경되는지 등)
+// 보통은 state에는 로그인과 관련된 데이터가 들어감 (카트 같은 데이터는 db와 관련됨)
+// action은 type을 보기 위한 것
 const reducer = (state = initalData, action) => {
   console.log(action);
 
   switch (action.type) {
     case "add_cart":
+      // ...state : state 값 전체, 그 중에서 cart 값을 변경한다는 의미
       return { ...state, cart: [...state.cart, action.payload] };
     case "remove_cart":
       return {
@@ -28,6 +34,7 @@ const store = createStore(reducer);
 
 function App() {
   return (
+    // react-redux에 있는 component
     <Provider store={store}>
       <div>shop</div>
       <hr />
